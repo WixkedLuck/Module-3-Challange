35a9b828b33206781e42539158fd4445c524d36c
David Hybner <dah1998@yahoo.com>
2022-07-19 23:30:14 -0400

    update arrays
    added more letters to the UpperArray and LowerArray

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ var generateBtn = document.querySelector("#generate");
 var Password;
 var pickone=false; 
 
-var UpperArray=["A","B","C","D","E","F","G","H","I","J","K"]
-var LowerArray=["a","b","c","d","e","f","g","h","i","j","k"]
+var UpperArray=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var LowerArray=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 var NumberArray=["0","1","2","3","4","5","6","7","8","9"]
 var SpecialArray=["!","<",">","@","#","$","*","^","+","="]
 var emptyArray=[]
