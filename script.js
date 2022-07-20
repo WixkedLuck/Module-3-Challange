0dd7bf1d28987a0bc9962e29cce98995455ecb8c
David Hybner <dah1998@yahoo.com>
2022-07-19 23:25:56 -0400

    Update functions and create var

--- script.js
+++ script.js
@@ -1,14 +1,103 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var Password;
+var pickone=false; 
 
+var UpperArray=["A","B","C","D","E","F","G","H","I","J","K"]
+var LowerArray=["a","b","c","d","e","f","g","h","i","j","k"]
+var NumberArray=["0","1","2","3","4","5","6","7","8","9"]
+var SpecialArray=["!","<",">","@","#","$","*","^","+","="]
+var emptyArray=[]
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//Function for length of Password
+function choseLength(){
+  var Passlength
+  //to break out of this loop this needs to be a number 
+ while (isNaN(Passlength)){
+  Passlength=window.prompt("Welcome! Please enter a number between 8 and 128 to start generating a password")
+  
+  if(isNaN(Passlength)){
+    alert("Please enter a NUMBER :')")
+  }
+  if(Passlength<8){
+    Passlength="g"
+    alert("Sorry Number is to Small")
+  }
+  if(Passlength>=128){
+    Passlength="g"
+    alert("Sorry Number is to Large")
+  }
+ }
 
-  passwordText.value = password;
+  return Passlength;
+}
+
+
+//create function
+function writePassword() {
+
+// Asks user for password length 
+var length= choseLength()
+
+
+//Ask user questions
+while(pickone==false){
+var Upper=window.confirm("Would you like Uppercase letters?")
+if(Upper){
+ alert("Great! Upper case letter will be used")
+ emptyArray=emptyArray.concat(UpperArray)
+ pickone=true;
+
+}
+var Lower=window.confirm("Would you like Lowercase letters?")
+if(Lower){
+  alert("Great! Lower case letter will be used")
+  emptyArray=emptyArray.concat(LowerArray)
+  pickone=true;
+}
+
+var Special=window.confirm("Would you like special characters?")
+if(Special){
+  alert("Great! Speical characters will be used")
+  emptyArray=emptyArray.concat(SpecialArray)
+  pickone=true;
+}
+var number=window.confirm("Would you like numbers?")
+if(number){
+  alert("Great! numbers will be used")
+  emptyArray=emptyArray.concat(NumberArray)
+  pickone=true;
+}
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+//set variables 
+var Password=""
+var tempRandom
+var ArrayUp
+//create for loop
+for(i=0; i<length; i++){
+   tempRandom=Math.floor(Math.random() *emptyArray.length);
+   ArrayUp=emptyArray[tempRandom];
+Password=Password+ArrayUp
+
+}
+
+
+
+
+  
+  var password = Password;
+  console.log(password)
+  var passwordText = document.querySelector("#password");
+  
+  passwordText.value = password;
+  
+
+}
+
+
+
+//button event listener 
+generateBtn.addEventListener("click", writePassword());
